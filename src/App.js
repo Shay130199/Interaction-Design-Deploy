56ee837d223f7ee123e78273254a544807d4cc5a
Shay-Lynn Dudhia <55833100+Shay130199@users.noreply.github.com>
2021-02-10 19:41:41 +0000

    Deploy my card react to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import './App.css';
+import Tweet from './Tweet';
+
+
+function App() {
+  return (
+    <div className= "app">
+      <Tweet />
+    </div>
+  );
+}
+
+export default App;
